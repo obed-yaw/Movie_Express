928a44119f82e7aab682e04f2c9b377774dc68c1
obed-yaw <obed.agyeman@ashesi.edu.gh>
2021-12-11 22:48:33 +1200

    Updated

--- movie_express.sql
+++ movie_express.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `movie_express`
 --
-
+create schema movie_express;
+use movie_express;
 -- --------------------------------------------------------
 
 --
