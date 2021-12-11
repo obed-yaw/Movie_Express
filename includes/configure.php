a5eea3a0911ea905cef311576ad2403102560a46
obed-yaw <90575031+obed-yaw@users.noreply.github.com>
2021-12-12 11:01:53 +1200

    Update configure.php

--- includes/configure.php
+++ includes/configure.php
@@ -1,5 +1,5 @@
 <?php
-  define( 'DB_HOST', 'http://20.108.182.178/' );// Set database host
+  define( 'DB_HOST', 'localhost' );// Set database host
   define( 'DB_USER', 'root' );// Set database user
   define( 'DB_PASS', 'Nuclearbomb2.' );// Set database password
   define( 'DB_NAME', 'movie_express' );// Set database name
